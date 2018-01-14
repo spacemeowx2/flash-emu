83da8fe6d86698a64d63475a5309396c52583a6e
imspace <imspace@vip.qq.com>
2018-01-14 19:09:43 +0800

    去掉大部分无用括号

--- src/compiler/ast2js.ts
+++ src/compiler/ast2js.ts
@@ -1,7 +1,33 @@
 import {Program, AST2Code, StatementType, BlockStatement, VariableDeclaration, ExpressionType} from './ast'
 import { Logger } from 'logger'
 const logger = new Logger('ast2js')
-
+const OpOrder: (string | string[])[] = [
+  ['=', '+=', '-=', '*=', '/=', '&=', '|=', '^='],
+  '||',
+  '&&',
+  '|',
+  '^',
+  '&',
+  ['==', '!=', '===', '!=='],
+  ['<', '<=', '>', '>='],
+  ['<<', '>>', '>>>'],
+  ['+', '-'],
+  ['*', '/', '%']
+]
+let OpPriority = new Map<string, number>()
+function initPriorityMap () {
+  let priority = 1
+  for (let i of OpOrder) {
+    if (typeof i === 'string') {
+      OpPriority.set(i, priority)
+    } else {
+      for (let j of i) {
+        OpPriority.set(j, priority)
+      }
+    }
+    priority++
+  }
+}
 type Line = [number, string]
 
 class Lines {
@@ -12,10 +38,10 @@ class Lines {
     if (typeof content === 'string') {
       this.l.push([indent, content])
     } else if (content instanceof Lines) {
-      this.l = this.l.concat(content.l)
+      this.l = this.l.concat(content.lwithIndent(indent))
     } else {
       for (let l of content) {
-        this.l = this.l.concat(l.l)
+        this.l = this.l.concat(l.lwithIndent(indent))
       }
     }
     return this
@@ -23,18 +49,37 @@ class Lines {
   toString () {
     return this.l.map(line => this.indentStr.repeat(line[0]) + line[1]).join('\n')
   }
+  private lwithIndent (indent: number) {
+    return this.l.map(i => {
+      const o: Line = [i[0] + indent, i[1]]
+      return o
+    })
+  }
 }
 
 export class AST2JS extends AST2Code {
   expr (e: ExpressionType): string {
     const expr = (e: ExpressionType) => this.expr(e)
     switch (e.type) {
-      case 'UnaryExpression':
-        return e.isPrefix ? `${e.op}(${expr(e.arg)})` : `(${expr(e.arg)})${e.op}`
-      case 'BinaryExpression':
-        return `(${expr(e.left)}) ${e.operator} (${expr(e.right)})`
+      case 'UnaryExpression': {
+        let arg = expr(e.arg)
+        if (e.arg.type === 'BinaryExpression') {
+          arg = `(${arg})`
+        }
+        return e.isPrefix ? `${e.operator}${arg}` : `${arg}${e.operator}`
+      }
+      case 'BinaryExpression': {
+        let left = expr(e.left)
+        let right = expr(e.right)
+        if (e.left.type === 'BinaryExpression') {
+          if (OpPriority.get(e.operator) > OpPriority.get(e.left.operator)) {
+            left = `(${left})`
+          }
+        }
+        return `${left} ${e.operator} ${right}`
+      }
       case 'AssignmentExpression':
-        return `${expr(e.left)} ${e.op} ${expr(e.right)}`
+        return `${expr(e.left)} ${e.operator} ${expr(e.right)}`
       case 'Identifier':
         return e.name
       case 'Literal':
@@ -61,11 +106,12 @@ export class AST2JS extends AST2Code {
         ret = ret.add(0, `if (${expr(n.test)})`)
                  .add(n.consequent.type === 'BlockStatement' ? 0 : 1, stmt(n.consequent, indent))
         if (n.alternate) {
-          ret = ret.add(0, `else ${stmt(n.alternate, indent)}`)
+          ret = ret.add(0, `else`)
+                   .add(n.alternate.type === 'BlockStatement' ? 0 : 1, stmt(n.alternate, indent))
         }
         return ret
       case 'JumpStatement':
-        return ret.add(0, `goto label${n.target}`)
+        return ret.add(0, `goto block${n.target}`)
       default:
         logger.error(`Empty stmt return ${n.type}`)
     }
